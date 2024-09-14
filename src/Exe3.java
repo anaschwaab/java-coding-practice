7b21b18c3c22726fb436b1bbe8bc1ebf4d4fde44
anaschwaab <goettensana@gmail.com>
2024-09-14 18:22:26 -0300

    exercício 3

---
+++ src/Exe3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exe3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        System.out.println("Digite um número:");
+        numero = sc.nextInt();
+
+        if (numero %2 != 0){
+            System.out.println("O número digitado é ímpar");
+        } else {
+            System.out.println("O número digitado é par");
+        }
+    }
+}
